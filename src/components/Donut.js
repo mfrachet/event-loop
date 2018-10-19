d3115dfb5bc83fa15e8eb5fc3cb4469312919c0b
mfrachet <marvin.frachet@zenika.com>
2018-10-19 19:05:15 +0200

    adding queuing + dequeuing

--- src/components/Donut.js
+++ src/components/Donut.js
@@ -1,22 +1,25 @@
-import React, { Fragment } from "react";
+import React, { Component } from "react";
+
 import "./Donut.css";
 
-export const Donut = ({ color = "#dddddd" }) => {
-  return (
-    <Fragment>
-      <div
-        className="lol"
-        style={{
-          backgroundColor: "red",
-          width: "200px",
-          height: "200px",
-          marginBottom: "-100px",
-          position: "absolute"
-        }}
-      />
+export class Donut extends Component {
+  componentDidMount() {
+    this.intervalId = setInterval(() => {
+      const { onHandle } = this.props;
+
+      return onHandle && onHandle();
+    }, 4000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.intervalId);
+  }
+
+  render() {
+    return (
       <div className="donut">
         <div className="donut-item" />
       </div>
-    </Fragment>
-  );
-};
+    );
+  }
+}
--- src/pages/EventLoop.js
+++ src/pages/EventLoop.js
@@ -1,19 +1,57 @@
 import React, { Component, Fragment } from "react";
 import { Title } from "../components/Title";
 import { Donut } from "../components/Donut";
+import { Cols, Col } from "../components/Cols";
 
 export class EventLoop extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = { queue: [] };
   }
 
+  componentDidMount() {
+    document.addEventListener("keypress", this.handleKeypress);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keypress", this.handleKeypress);
+  }
+
+  handleKeypress = e => {
+    if (e.key === " ") {
+      const { queue } = this.state;
+
+      queue.push("Some call");
+
+      this.setState({ queue });
+    }
+  };
+
+  dequeue = () => {
+    const { queue } = this.state;
+
+    queue.shift();
+
+    this.setState({ queue });
+  };
+
   render() {
+    const { queue } = this.state;
     return (
       <Fragment>
         <Title>Event loop</Title>
-        <Donut />
+
+        <Cols>
+          <Col>
+            <Donut onHandle={this.dequeue} />
+          </Col>
+          <Col>
+            {queue.map((name, index) => (
+              <div key={`${name}-${index}`}>{name}</div>
+            ))}
+          </Col>
+        </Cols>
       </Fragment>
     );
   }
