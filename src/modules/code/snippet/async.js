c9568484e2c18560f22d1be2e9a7e7f99e4090d1
mfrachet <marvin.frachet@zenika.com>
2018-10-24 11:57:44 +0200

    refactor task queue

--- src/modules/code/snippet/async.js
+++ src/modules/code/snippet/async.js
@@ -14,10 +14,10 @@ export const steps = {
     funcName: "main"
   },
   2: {
-    line: 3
+    line: 2
   },
   3: {
-    line: 4,
+    line: 3,
     funcName: "setTimeout"
   },
   4: {
@@ -26,11 +26,11 @@ export const steps = {
     action: "enqueue"
   },
   5: {
-    line: 3,
+    line: 2,
     action: "pop"
   },
   6: {
-    line: 4,
+    line: 3,
     funcName: "setTimeout"
   },
   7: {
@@ -43,25 +43,25 @@ export const steps = {
     action: "pop"
   },
   9: {
-    line: 8,
+    line: 9,
     action: "pop"
   },
   10: {
-    line: 8,
+    line: 9,
     action: "dequeue",
     funcName: "callback"
   },
   11: {
-    line: 8,
+    line: 9,
     action: "pop"
   },
   12: {
-    line: 8,
+    line: 9,
     action: "dequeue",
     funcName: "callback"
   },
   13: {
-    line: 8,
+    line: 9,
     action: "pop"
   }
 };
